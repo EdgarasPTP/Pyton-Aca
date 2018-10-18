7086b63c9673cb686eb761d1b4c7bd476e4ccc04
EdgarasPTP <44160483+EdgarasPTP@users.noreply.github.com>
2018-10-18 16:56:14 +0300

    Add files via upload

---
+++ Module 1.py
@@ -0,0 +1,104 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[2]:
+
+
+# this is a "Hello World!" for Python Academy
+print("It is Wednesday my friends!")
+
+
+# In[4]:
+
+
+# insert a new cell
+print("After edit save!")
+
+
+# In[5]:
+
+
+# add print() with the message showing the keyboard Shortcut to save
+print("Ctrl + s")
+
+
+# In[6]:
+
+
+# enter a string in the print() function using single quotes
+print('I used single quotes!')
+
+
+# In[7]:
+
+
+# [ ] enter a string in the print() function using double quotes
+print("i used double quotes!")
+
+
+# In[9]:
+
+
+# [ ] print an Integer
+print(-69)
+# [ ] print a strings made of Integer characters
+print("69")
+
+
+# In[22]:
+
+
+# print the variable current_msg
+current_msg = "Imma snake"
+print(current_msg)
+
+
+# In[25]:
+
+
+# assign a new string to current_msg
+current_msg = "New variable!"
+
+
+# In[26]:
+
+
+# print the variable current_msg again
+print(current_msg)
+
+
+# In[32]:
+
+
+# [ ] assign a string value to a variable student_name and print the value of variable student_name
+student_name = "ED"
+print(student_name)
+
+
+# In[31]:
+
+
+# [ ] assign the student_name variable a different string value (a different name) and print the value of variable student_name
+student_name = 'EDDIE'
+print(student_name)
+# [ ] assign a 3rd different string value, to the variable name and print the value of variable name
+student_name = 'EDDY'
+print(student_name)
+
+
+# In[33]:
+
+
+# [ ] assigning a value to a variable called bucket and print the value of bucket 
+bucket = "Spoon"
+print(bucket)
+# [ ] assign an Integer value (no quotes) to the variable bucket and print the value of bucket 
+bucket = 12
+print(bucket)
+
+
+# In[ ]:
+
+
+
+
