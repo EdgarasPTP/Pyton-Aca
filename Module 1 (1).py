a00d46c46be6868973c0d4df2eb9407ebfe7eace
EdgarasPTP <44160483+EdgarasPTP@users.noreply.github.com>
2018-10-26 11:06:40 +0300

    Add files via upload

---
+++ Module 1 (1).py
@@ -0,0 +1,598 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[2]:
+
+
+# this is a "Hello World!" for Python Academy
+print("It is Wednesday my friends!")
+
+
+# In[4]:
+
+
+# insert a new cell
+print("After edit save!")
+
+
+# In[5]:
+
+
+# add print() with the message showing the keyboard Shortcut to save
+print("Ctrl + s")
+
+
+# In[6]:
+
+
+# enter a string in the print() function using single quotes
+print('I used single quotes!')
+
+
+# In[7]:
+
+
+# [ ] enter a string in the print() function using double quotes
+print("i used double quotes!")
+
+
+# In[9]:
+
+
+# [ ] print an Integer
+print(-69)
+# [ ] print a strings made of Integer characters
+print("69")
+
+
+# In[22]:
+
+
+# print the variable current_msg
+current_msg = "Imma snake"
+print(current_msg)
+
+
+# In[25]:
+
+
+# assign a new string to current_msg
+current_msg = "New variable!"
+
+
+# In[26]:
+
+
+# print the variable current_msg again
+print(current_msg)
+
+
+# In[32]:
+
+
+# [ ] assign a string value to a variable student_name and print the value of variable student_name
+student_name = "ED"
+print(student_name)
+
+
+# In[31]:
+
+
+# [ ] assign the student_name variable a different string value (a different name) and print the value of variable student_name
+student_name = 'EDDIE'
+print(student_name)
+# [ ] assign a 3rd different string value, to the variable name and print the value of variable name
+student_name = 'EDDY'
+print(student_name)
+
+
+# In[33]:
+
+
+# [ ] assigning a value to a variable called bucket and print the value of bucket 
+bucket = "Spoon"
+print(bucket)
+# [ ] assign an Integer value (no quotes) to the variable bucket and print the value of bucket 
+bucket = 12
+print(bucket)
+
+
+# In[2]:
+
+
+# [ ] show the type after assigning bucket = a whole number value such as 16 
+bucket = 69
+type(bucket)
+
+
+# In[5]:
+
+
+# [ ] show  the type after assigning bucket = a word in "double quotes"
+bucket = "Word"
+type (bucket)
+
+
+# In[6]:
+
+
+# [ ] display the type of 'single quoting' (use single quotes)
+type('Single')
+
+
+# In[7]:
+
+
+# [ ] display the type of "double quoting" (use double quotes)
+type("Pair")
+
+
+# In[8]:
+
+
+# [ ] display the type of "12" (use quotes)
+type ("12")
+
+
+# In[9]:
+
+
+# [ ] display the type of 12 (no quotes)
+type (12)
+
+
+# In[10]:
+
+
+# [ ] display the type of -12 (no quotes)
+type (-12)
+
+
+# In[11]:
+
+
+# [ ] display the type of 12.0 (no quotes)
+type (12.0)
+
+
+# In[12]:
+
+
+# [ ] display the type of 1.55
+type (1.55)
+
+
+# In[13]:
+
+
+# [ ] find the type of the type(3) statement (no quotes) - just for fun
+type (type(3))
+
+
+# In[14]:
+
+
+# [ ] add 3 integer numbers
+3+5+6
+
+
+# In[15]:
+
+
+# [ ] add a float number and an integer number
+3+5.6
+
+
+# In[17]:
+
+
+# [ ] Add the string "This notebook belongs to " and a string with your first name
+"This notebook belongs to " + "Edgaras"
+
+
+# In[18]:
+
+
+# [ ] Create variables sm_number and big_number and assign numbers then add the numbers
+sm_number = 5.1
+big_number = 69
+sm_number + big_number
+
+
+# In[19]:
+
+
+# [ ] assign a string value to the variable first_name and add to the string ", remember to save the notebook frequently"
+first_name = "Jeff"
+first_name + ", remember to save the notebook frequently"
+
+
+# In[22]:
+
+
+# [ ] perform string addition in the variable named new_msg (add a string to "my favorite food is ")
+new_msg = "My favorite food is " + "Catnip"
+print(new_msg)
+
+
+# In[25]:
+
+
+# [ ] perform Integer addition in the variable named new_msg (add 2 or more Integers)
+new_sum =   0 + 1 + 2
+print(new_sum)
+
+
+# In[28]:
+
+
+# [ ] create and print a new string variable, new_msg_2, that concatenates new_msg + a literal string
+new_msg_2 = new_msg + ", but my cat likes it more."
+print (new_msg_2)
+
+
+# In[34]:
+
+
+# [ ] review and run the code - then fix any Errors total_cost = 3 + "45"; print(total_cost)
+total_cost = 3 + 45
+print(total_cost)
+
+
+# In[35]:
+
+
+# [ ] review and run the code - then fix any Errors school_num = 123; print("the street number of Central School is " + school_num)
+school_num = "123"
+print("the street number of Central School is " + school_num)
+
+
+# In[36]:
+
+
+# [ ] Read and run the code - write a hypothesis for what you observe adding float + int
+#  [ ] HYPOTHESIS: The outcome after adding float + int will always be a float
+
+print(type(3.3))
+print(type(3))
+print(3.3 + 3)
+
+
+# In[37]:
+
+
+# [ ] repair the syntax error: print('my socks do not match")
+print("my socks do not match")
+
+
+# In[38]:
+
+
+# [ ] repair the NameError: pront("my socks match now") 
+print("my socks match now") 
+
+
+# In[39]:
+
+
+# [ ] repair the syntax error: print"Save the notebook frequently")
+print("Save the notebook frequently")
+
+
+# In[40]:
+
+
+# [ ] repair the NameError: student_name = "Alton"; print(STUDENT_NAME)
+student_name = "Alton"
+print(student_name)
+
+
+# In[41]:
+
+
+# [ ] repair the TypeError: total = 3; print(total + " students are signed up for tutoring")
+total = "3"
+print(total + " students are signed up for tutoring")
+
+
+# In[42]:
+
+
+# create # [ ] flying bird character art
+print ("_         _")
+print (" \       / ")
+print ("  \ . . /  ")
+print ("     V   ")
+
+
+# In[43]:
+
+
+# [ ] capital letter "E" character art
+print ("*****")
+print ("*")
+print ("***")
+print ("*")
+print ("*****")
+
+
+# Python Practice
+
+# In[1]:
+
+
+# [ ] print 'Hello!' and remember to save notebook!
+print('Hello!')
+
+
+# In[2]:
+
+
+# run the cell below: use Ctrl + Enter; use Shift + Enter
+print('watch for the cat')
+
+
+# In[1]:
+
+
+# [ ] print your name
+# [ ] print "is using Python"
+
+print ("Edgaras")
+print ("is using Python")
+
+
+# In[2]:
+
+
+# [ ] create a variable your_name and assign it a sting containing your name
+#[ ] print your_name
+your_name= "Edgaras"
+print (your_name)
+
+
+# In[3]:
+
+
+# [ ] create variables and assign values for: favorite_song, shoe_size, lucky_number
+# [ ] print the value of each variable favorite_song, shoe_size, and lucky_number
+favorite_song="Never gonna give you up"
+shoe_size= "46"
+lucky_number= "69"
+print (favorite_song)
+print (shoe_size)
+print (lucky_number)
+
+
+# In[6]:
+
+
+# [ ] print favorite_song with description
+# [ ] print shoe_size with description
+# [ ] print lucky_number with description
+print ("Rick Rolls - " + favorite_song)
+print (shoe_size + " in EU shoe size scale")
+print (lucky_number + " - it spreads the luck")
+
+
+# In[7]:
+
+
+# assign favorite_lucky_shoe using [ ] make a single string (sentence) in a variable called favorite_lucky_shoe using string addition with favorite_song, shoe_size, lucky_number variables and other strings as needed
+# [ ] print the value of the favorite_lucky_shoe variable string
+favorite_lucky_shoe= favorite_song + " until the age of " + shoe_size + " or maybe " + lucky_number
+print (favorite_lucky_shoe)
+
+
+# In[9]:
+
+
+# print a diagonal using "*"
+print("*")
+print(" *")
+print("  *")
+print("   *")
+print("    *")
+
+
+# In[10]:
+
+
+# [ ] rectangle using "*"
+print("*****")
+print("*   *")
+print("*   *")
+print("*****")
+
+
+# In[1]:
+
+
+# [ ] smiley using "*"
+print("   * *   ")
+print("   * *   ")
+print("*       *")
+print(" *******")
+
+
+# In[24]:
+
+
+# [ ] display the type of 'your name' (use single quotes)
+type ('your name')
+
+
+# In[25]:
+
+
+# [ ] display the type of "save your notebook!" (use double quotes)
+type ("save your notebook!")
+
+
+# In[26]:
+
+
+# [ ] display the type of "25" (use quotes)
+type ("25")
+
+
+# In[27]:
+
+
+# [ ] display the type of "save your notebook " + 'your name'
+type ("save your notebook " + 'your name')
+
+
+# In[28]:
+
+
+# [ ] display the type of 25 (no quotes)
+type (25)
+
+
+# In[29]:
+
+
+# [ ] display the type of 25 + 10 
+type (25 + 10)
+
+
+# In[30]:
+
+
+# [ ] display the type of 1.55
+type (1.55)
+
+
+# In[31]:
+
+
+# [ ] display the type of 1.55 + 25
+type (1.55 + 25)
+
+
+# In[32]:
+
+
+# assignments ***RUN THIS CELL*** before starting the section
+
+student_name = "Gus"
+student_age = 16
+student_grade = 3.5
+student_id = "ABC-000-000"
+
+
+# In[39]:
+
+
+# [ ] display the current type of the variable student_name
+type (student_name)
+
+
+# In[40]:
+
+
+# [ ] display the type of student_age
+type (student_age)
+
+
+# In[41]:
+
+
+# [ ] display the type of student_grade
+type (student_grade)
+
+
+# In[42]:
+
+
+# [ ] display the type of student_age + student_grade
+type (student_age + student_grade)
+
+
+# In[43]:
+
+
+# [ ] display the current type of student_id
+type (student_id)
+
+
+# In[44]:
+
+
+# assign new value to student_id 
+student_id = "BBC-007-101"
+
+# [ ] display the current of student_id
+type (student_id)
+
+
+# In[45]:
+
+
+# [ ] create integer variables (x, y, z) and assign them 1-3 digit integers (no decimals - no quotes)
+x= 1
+y= 9
+z= 59
+# [ ] create an integer variable named xyz_sum equal to the sum of x, y, and z
+xyz_sum = x + y + z
+# [ ] print the value of xyz_sum
+print (xyz_sum)
+
+
+# In[46]:
+
+
+# [ ] fix the error print("Hello World!"") 
+print("Hello World!")
+
+
+# In[47]:
+
+
+# [ ] fix the error print(strings have quotes and variables have names)
+print("strings have quotes and variables have names")
+
+
+# In[48]:
+
+
+# [ ] fix the error print( "I have $" + 5)
+print( "I have $" + "5")
+
+
+# In[49]:
+
+
+# [ ] fix the error print('always save the notebook")
+print("always save the notebook")
+
+
+# In[52]:
+
+
+# [ ] ASCII ART - Display first name or initials as ASCII Art
+print ("*****  ****")
+print ("*      *  *")
+print ("***    ****")
+print ("*      *")
+print ("*****  *")
+
+
+# In[6]:
+
+
+# [ ] ASCII ART Challenge: insert an additional code cell to make an ASCII picture
+print ("    .")
+print (".__/ \__.")
+print (" \     /")
+print (' /_   _\ ')
+print (" ' \ /  '")
+print ("    '")
+
+
+# In[ ]:
+
+
+
+
